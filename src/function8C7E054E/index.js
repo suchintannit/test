87f30b7fc3333a9b024b296b5b1ef29d75c62274
suchintannit <suchintan.mishra@gmail.com>
2018-12-31 15:57:29 +0530

    Update index.js

--- src/function8C7E054E/index.js
+++ src/function8C7E054E/index.js
@@ -1,5 +1,17 @@
-exports.handler = async message => {
-  console.log(message);
+const fs = require('fs');
+exports.handler = async event => {
+  // Log http request
+  console.log(event);
 
-  return {};
-}
+  const responseBody = fs.readFileSync('./welcome.html', 'utf8');
+  // Build an HTTP response.
+  const response = {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'text/html'
+    },
+    body: responseBody
+  };
+
+  return response;
+};
